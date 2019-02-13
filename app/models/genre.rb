35e40f956d47de953eeb239ededdfa99a8f9b8bd
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-13 23:05:18 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.name.all {|artist| artist.name}
+    self.name.collect {|artist| artist.name}
   end
 end
